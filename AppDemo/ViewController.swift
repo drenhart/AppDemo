58a7c4f75aa305ff535abca5e6804c8060089d82
mingchang <drenhart@163.com>
2022-04-20 22:42:47 +0800

    Initial Commit

---
+++ AppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDemo
+//
+//  Created by mc Lee on 2022/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
